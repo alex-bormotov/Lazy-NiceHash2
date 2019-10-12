c54b99d42dea525763cffb3008bcf7618e73b43b
Alex Bormotov <alex.bormotov.work@gmail.com>
2019-10-12 20:33:11 +0300

    access only for LIST_OF_ADMINS @restricted

--- bot.py
+++ bot.py
@@ -4,8 +4,12 @@ import requests
 import nicehash
 import http.client
 from time import sleep
+from functools import wraps
 from datetime import datetime, timedelta
 from telegram.ext import Updater, CommandHandler
+
+
+last_start = datetime.utcnow()
 
 
 def get_config():
@@ -24,86 +28,85 @@ try:
 except Exception as e:
     print(str(e))
 
-last_start = datetime.utcnow()
-
 
 def get_balance(coin):
     return float(private_api.get_accounts()[coin]["balance"])
 
 
 # telegram func:
+
+# LIST_OF_ADMINS = [12345678, 87654321]
+LIST_OF_ADMINS = get_config()["telegram_admin_id"]
+
+
+def restricted(func):
+    @wraps(func)
+    def wrapped(update, context, *args, **kwargs):
+        user_id = update.effective_user.id
+        if user_id not in LIST_OF_ADMINS:
+            # print("Unauthorized access denied for {}.".format(user_id))
+            context.bot.send_message(
+                chat_id=LIST_OF_ADMINS[0],
+                text=("Unauthorized access denied for {}.".format(user_id)),
+            )
+            return
+        return func(update, context, *args, **kwargs)
+
+    return wrapped
+
+
 def start(update, context):
     context.bot.send_message(
         chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
     )
 
 
+@restricted
 def balance(update, context):
-    coin = " ".join(context.args).upper()
-    if coin not in ["BTC", "ETH", "XRP", "BCH", "LTC", "ZEC"]:
-        balance = "Enter correct coin name, like /balance btc"
-    if coin == "BTC":
-        balance = " You have " + str(get_balance(0)) + "BTC"
-    if coin == "ETH":
-        balance = " You have " + str(get_balance(1)) + "ETH"
-    if coin == "XRP":
-        balance = " You have " + str(get_balance(2)) + "XRP"
-    if coin == "BCH":
-        balance = " You have " + str(get_balance(3)) + "BCH"
-    if coin == "LTC":
-        balance = " You have " + str(get_balance(4)) + "LTC"
-    if coin == "ZEC":
-        balance = " You have " + str(get_balance(5)) + "ZEC"
-    context.bot.send_message(chat_id=update.effective_chat.id, text=balance)
+    try:
+        coin = " ".join(context.args).upper()
+        if coin not in ["BTC", "ETH", "XRP", "BCH", "LTC", "ZEC"]:
+            balance = "Enter correct coin name, like /balance btc"
+        if coin == "BTC":
+            balance = " You have " + str(get_balance(0)) + "BTC"
+        if coin == "ETH":
+            balance = " You have " + str(get_balance(1)) + "ETH"
+        if coin == "XRP":
+            balance = " You have " + str(get_balance(2)) + "XRP"
+        if coin == "BCH":
+            balance = " You have " + str(get_balance(3)) + "BCH"
+        if coin == "LTC":
+            balance = " You have " + str(get_balance(4)) + "LTC"
+        if coin == "ZEC":
+            balance = " You have " + str(get_balance(5)) + "ZEC"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=balance)
+    except Exception as e:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=str(e))
+
+
+@restricted
+def trade(update, context):
+    try:
+
+        context.bot.send_message(
+            chat_id=update.effective_chat.id, text=" ".join(context.args).upper()
+        )
+
+        # amount = get_balance(0)  # !!!
+
+        # new_buy_market_order = private_api.create_exchange_buy_market_order(
+        #     public_api.get_exchange_markets_info()["symbols"][1]["symbol"], amount
+        # )
+    except Exception as e:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=str(e))
 
 
 updater = Updater(get_config()["telegram_bot_token"], use_context=True)
 updater.dispatcher.add_handler(CommandHandler("start", start))
 updater.dispatcher.add_handler(CommandHandler("balance", balance))
+updater.dispatcher.add_handler(CommandHandler("trade", trade))
 
 
 updater.start_polling()
 updater.idle()
 # end telegram func
-
-# def make_trade():
-#
-#     discord_send_message(f"BTC balance before trade is {get_balance(0)}")
-#     discord_send_message(f"XRP balance before trade is {get_balance(2)}")
-#
-#     try:
-#         new_buy_market_order = private_api.create_exchange_buy_market_order(
-#             public_api.get_exchange_markets_info()["symbols"][1]["symbol"], get_balance(0)
-#         )
-#     except Exception as e:
-#         discord_send_message(str(e))
-#
-#     discord_send_message(f'Bought {new_buy_market_order["executedQty"]} XRP')
-#     discord_send_message(f"Total XRP balance after trade is {get_balance(2)}")
-#
-#
-# def main():
-#
-#     global last_start
-#
-#     discord_send_message("Starting ...")
-#
-#     if get_balance(0) > 0:
-#         make_trade()
-#
-#     while True:
-#         if (
-#             last_start + timedelta(hours=float(get_config()["exchange_period_hours"]))
-#             < datetime.utcnow()
-#         ):
-#             make_trade()
-#             last_start = datetime.utcnow()
-#             time.sleep(3600)
-#             continue
-#         else:
-#             time.sleep(3600)
-#             continue
-#
-#
-# if __name__ == "__main__":
-#     main()
